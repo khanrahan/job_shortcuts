3fcd074fddc4ad67497d2ad87257fd0e8fc98b29
Kieran Hanrahan <kieran@knhn.me>
2018-05-03 23:17:01 -0400

    functional on laptop

--- job_shortcuts.py
+++ job_shortcuts.py
@@ -5,15 +5,16 @@ import os
 JOB_TREE = "/jobs/ads/test_test_j1111111/"
 DESTINATION = "/Users/kieran/Python/job_shortcuts"
 
-job_name = os.path.basename(os.path.normpath(job_tree))
-dirs = [i for i  in os.listdir(job_tree)]
-materials = [i for i in dirs if i.isupper()]
+job_name = os.path.basename(os.path.normpath(JOB_TREE))
+dirs = [i for i  in os.listdir(JOB_TREE)]
+materials = [i for i in dirs if i.isupper()] 
 
-def find_shots():
-	
-
-def create_link():
-	
-
-for i in materials:
-    shots = os.listdir(os.path.join(JOB_TREE, i)
+for x in materials:
+    shots = os.listdir(os.path.join(JOB_TREE, x))
+    shot_numbers = [i[2:] for i in shots]
+    for y in shots:
+        source = os.path.join(JOB_TREE, x, y, "_".join(["n", y[2:]]),
+        "live_action") 
+        link_dir = os.path.join(DESTINATION, job_name, x, y, "nuke")
+        os.makedirs(link_dir)
+        os.symlink(source, os.path.join(link_dir, "live_action"))
