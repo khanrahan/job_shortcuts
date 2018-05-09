b52af3f53798b17526548358621251c49f448c73
Kieran Hanrahan <kieran@knhn.me>
2018-05-08 23:17:14 -0400

    cleaned up code based on pylint

--- job_shortcuts.py
+++ job_shortcuts.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 
 JOB_TREE = "/jobs/ads/test_test_j1111111/"
 DESTINATION = "/Users/kieran/Python/job_shortcuts"
 
-job_name = os.path.basename(os.path.normpath(JOB_TREE))
-dirs = [i for i  in os.listdir(JOB_TREE)]
-materials = [i for i in dirs if i.isupper()] 
+JOB_NAME = os.path.basename(os.path.normpath(JOB_TREE))
+DIRS = [i for i  in os.listdir(JOB_TREE)]
+MATERIALS = [i for i in DIRS if i.isupper()]
 
-for x in materials:
+for x in MATERIALS:
     shots = os.listdir(os.path.join(JOB_TREE, x))
     shot_numbers = [i[2:] for i in shots]
     for y in shots:
         source = os.path.join(JOB_TREE, x, y, "_".join(["n", y[2:]]),
-        "live_action") 
-        link_dir = os.path.join(DESTINATION, job_name, x, y, "nuke")
+                              "live_action")
+        link_dir = os.path.join(DESTINATION, JOB_NAME, x, y, "nuke")
         os.makedirs(link_dir)
         os.symlink(source, os.path.join(link_dir, "live_action"))
